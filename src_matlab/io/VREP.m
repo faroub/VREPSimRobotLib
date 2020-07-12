a3c5637c35ce94ca681dd4fd7bdd4994cffbcead
Farid Oubbati <f.oubbati@yahoo.f>
2020-07-12 15:04:06 +0200

    add io folder

---
+++ src_matlab/io/VREP.m
@@ -0,0 +1,19 @@
+classdef VREP
+    %   VREP: is a V-REP simulator communications object
+    %   This class handles the interface to the simulator and 
+    %   low-level object handle operations
+    %
+    %   A VREP object holds all information related to 
+    %   the state of a connection to an instance of the 
+    %   V-REP simulator running on this or a networked computer. 
+    %   It also allows the creation of references to other objects/models 
+    %   in V-REP which can be manipulated in MATLAB
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
